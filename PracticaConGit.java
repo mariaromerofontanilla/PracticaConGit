833622f99e6c1c2d132c7f523707e96429a1c121
mariaromerofontanilla <145675441+mariaromerofontanilla@users.noreply.github.com>
2024-02-14 21:06:09 +0100

    Este es el primer commit de este ejercicio

---
+++ PracticaConGit.java
@@ -0,0 +1,17 @@
+package practicaConGit;
+
+import java.util.Scanner;
+
+public class PracticaConGit {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Introduzca su nombre: ");
+		String nombre = sc.nextLine();
+		
+		System.out.println(nombre);
+
+	}
+
+}
